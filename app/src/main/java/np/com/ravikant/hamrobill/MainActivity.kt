71dc82298a0b652c0064a5214a68772efe69b9c9
Ravikant Paudel <developer.paudel@gmail.com>
2024-03-18 18:09:38 +0545

    :tada: Initial Commit

---
+++ app/src/main/java/np/com/ravikant/hamrobill/MainActivity.kt
@@ -0,0 +1,97 @@
+package np.com.ravikant.hamrobill
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import np.com.ravikant.hamrobill.ui.theme.HamroBillTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HamroBillTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                colors = TopAppBarDefaults.topAppBarColors(
+                                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                    titleContentColor = MaterialTheme.colorScheme.primary,
+                                ),
+                                title = {
+                                    Text("Events")
+                                }
+                            )
+                        },
+                    ) { innerPadding ->
+                        EventList(
+                            modifier = Modifier
+                                .padding(innerPadding),
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun EventList(modifier: Modifier = Modifier) {
+    LazyColumn (modifier = modifier){
+        items(8) { index ->
+            EventRow(index)
+        }
+    }
+}
+
+
+@Composable
+fun EventRow(index: Int) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Column(Modifier.padding(all = 16.dp)) {
+            Text(text = "${index + 1}.  Sat 16th March - Cost")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text("Location")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text("Total")
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HamroBillTheme {
+        EventList()
+    }
+}
