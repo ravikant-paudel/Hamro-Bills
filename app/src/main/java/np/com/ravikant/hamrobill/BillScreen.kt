8f16b379f061401b8575b466a300e2e032dbd88d
Ravikant Paudel <developer.paudel@gmail.com>
2024-03-19 18:11:55 +0545

    :ambulance: Fix analyser

--- app/src/main/java/np/com/ravikant/hamrobill/BillScreen.kt
+++ app/src/main/java/np/com/ravikant/hamrobill/BillScreen.kt
@@ -1,16 +1,6 @@
 package np.com.ravikant.hamrobill
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 
@@ -18,7 +8,7 @@ import androidx.navigation.compose.rememberNavController
 @Composable
 fun HamroBillApp() {
     val rootController = rememberNavController()
-    val navigationBarController = rememberNavController()
+//    val navigationBarController = rememberNavController()
 
     NavHost(
         navController = rootController,
